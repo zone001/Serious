d40f66b93c61e5ff6565bb363edb07cce5998968
m.polish <m.polish1@gmail.com>
2021-11-08 21:20:05 +0100

    elo

--- Serious/src/pl/adsem/Main.java
+++ Serious/src/pl/adsem/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Marcin ");
+        System.out.println("Marcin ech ta ");
     }
 }
